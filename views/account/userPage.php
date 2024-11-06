1d830992c636f31b588bb0d50d16ba850e2cbdf5
khacduy <khacduy584@gmail.com>
2024-11-06 17:42:10 +0700

    update main

--- views/account/userPage.php
+++
@@ -1,6 +0,0 @@
-<?php include 'views/layouts/header.php'; ?>
-<div class="text-center">
-    <h2>Welcome, <?= htmlspecialchars($_SESSION['user']['user_name']); ?>!</h2>
-    <p>Your role is: <?= $_SESSION['user']['user_role'] == 1 ? 'Admin' : 'User'; ?></p>
-</div>
-<?php include 'views/layouts/footer.php'; ?>
