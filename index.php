66267a1e31720a3da1dc490060883b4f49003f09
khacduy <khacduy584@gmail.com>
2024-11-11 19:12:13 +0700

    a

--- index.php
+++ index.php
@@ -105,6 +105,16 @@ switch ($mod) {
                 break;
             }
         }
+    case 'product':
+        require_once('Controllers/ProductController.php');
+        $controller_obj = new ProductController();
+        $controller_obj->list();
+        break;
+    // case 'checkout':
+    //     require_once('Controllers/CheckoutController.php');
+    //     $controller_obj = new CheckoutController();
+    //     $controller_obj->list();
+    //     break;
     default:
         require_once('Controllers/HomeController.php');
         $controller_obj = new Homecontroller();
