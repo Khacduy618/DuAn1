b4c5fa27701104b1b02419b236ce2c5dcc574782
khacduy <khacduy584@gmail.com>
2024-11-19 14:31:19 +0700

    update admin

---
+++ Admin/MVC/Views/admin/menu.php
@@ -0,0 +1,97 @@
+<ul class="navbar-nav">
+    <li class="nav-item">
+        <a href="#" class="nav-link">
+            <span class="icon">
+                <div class="user">
+                </div>
+            </span>
+            <span class="title"><?=$_SESSION['login']['user_name']?></span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="../trangchinh/">
+            <span class="icon">
+                <ion-icon name="home-outline"></ion-icon>
+            </span>
+            <span class="title">Trang chủ</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="../danhmuc/">
+            <span class="icon">
+                <ion-icon name="list-circle-outline"></ion-icon>
+            </span>
+            <span class="title">Danh mục</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="../loaisanpham/">
+            <span class="icon">
+                <ion-icon name="albums-outline"></ion-icon>
+            </span>
+            <span class="title">Loại sản phẩm</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="../sanpham/">
+            <span class="icon">
+                <ion-icon name="pricetags-outline"></ion-icon>
+            </span>
+            <span class="title">Sản phẩm</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="../binhluan/">
+            <span class="icon">
+                <i class="fa-regular fa-comment fa-xl"></i>
+            </span>
+            <span class="title">Bình luận</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="../donhang/">
+            <span class="icon">
+                <i class="fa-solid fa-bag-shopping fa-xl"></i>
+            </span>
+            <span class="title">Đơn hàng</span>
+        </a>
+    </li>
+    <?php
+        if($_SESSION['login']['user_role'] == 1 ){
+    ?>
+    <li class="nav-item">
+        <a class="nav-link" href="../nguoidung/">
+            <span class="icon">
+                <ion-icon name="people-outline"></ion-icon>
+            </span>
+            <span class="title">Khách hàng</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="../vaitro/">
+            <span class="icon">
+                <ion-icon name="accessibility-outline"></ion-icon>
+            </span>
+            <span class="title">Vai trò</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="../thongke/">
+            <span class="icon">
+                <i class="fa-solid fa-square-poll-vertical fa-xl"></i>
+            </span>
+            <span class="title">Thống kê</span>
+        </a>
+    </li>
+    <?php
+    }
+    ?>
+    <li class="nav-item">
+        <a class="nav-link" href="<?=$SITE_URL?>/trangchinh">
+            <span class="icon">
+                <ion-icon name="log-out-outline"></ion-icon>
+            </span>
+            <span class="title">Trang chủ SHOP</span>
+        </a>
+    </li>
+</ul>
